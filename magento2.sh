449f28226af735d439d56fd5067ff4cc65c9b83d
Philippe Mendes Campeau <campeau.philippe@gmail.com>
2019-04-23 08:44:58 -0400

    Update magento2.sh

--- magento2.sh
+++ magento2.sh
@@ -5,8 +5,19 @@ _magento()
     _get_comp_words_by_ref -n : cur prev words
     
     if [[ "${#cur}" == "0" ]] || [[ "${words[1]}" != "bin/magento" ]]; then return 1;fi;
+    
+    # Absolute path to this script, e.g. /home/user/bin/foo.sh
+    SCRIPT=$(readlink -f "$0")
+    # Absolute path this script is in, thus /home/user/bin
+    SCRIPTPATH=$(dirname "$SCRIPT")
+    GENERATED_PATH=$SCRIPTPATH"/generated/auto-complete.txt"
 
-    opts=$(${words[0]} bin/magento list --raw | sed -e 's/ \+.*$//')
+    if [[ -f $GENERATED_PATH ]]; then
+        opts=$(cat $GENERATED_PATH)
+    else
+        opts=$(${words[0]} bin/magento list --raw | sed -e 's/ \+.*$//')
+        echo "$opts" > $GENERATED_PATH
+    fi
 
     COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
     __ltrim_colon_completions $cur
